ccc737f9149cd031cd5d314cc381f98d4f4b056d
SharmaVibhu <68889580+SharmaVibhu@users.noreply.github.com>
2020-11-24 10:08:36 +0530

    Training Day - 13( 23 -11 -2020 )
    Training Day - 13( 23 -11 -2020 )

---
+++ StaticBlockEx.java
@@ -0,0 +1,9 @@
+public class StaticBlockEx {
+	static {
+		System.out.println("Static Block");
+	}
+	public static void main(String args[]) {
+		System.out.println("Static Method");
+	}
+
+}
--- Test.java
+++ Test.java
@@ -1,46 +1,29 @@
-class Vehicle{
-String car_name;
-int registration_number;
-int highest_speed;
-int gear;
-int price;
-String brand;
-static String manufactured;
+class MySql{
+	static {
+		System.out.println("My Sql Static Block:");
+	}
+	public MySql(){
+	System.out.println("MySql Constructor");
+	}
 }
-public class Test{
-public static void main(String[] args)
-{
-Vehicle creta = new Vehicle();
-creta.car_name= "Creta";
-creta.registration_number=1001;
-creta.highest_speed=180;
-creta.gear=6;
-creta.price=760000;
-creta.brand="Hyundai";
-Vehicle.manufactured="Europe";
-System.out.println("Car:" + creta.car_name);
-System.out.println("registration_number:" + creta.registration_number);
-System.out.println("highest_speed:" + creta.highest_speed);
-System.out.println( "gear:" + creta.gear);
-System.out.println("price:" + creta.price);
-System.out.println("brand:" + creta.brand );
-System.out.println("manufactured:" + Vehicle.manufactured);
-System.out.println();
+class Oracle{
+	static {
+		System.out.println("Oracle static block");
+	}
+	public Oracle(){
+		System.out.println("Oracle Constructor");
+	}
+}
+public class Test {
+	public static void main(String args[]) throws Exception {
+		System.out.println("Static Method");
+		Class C=Class.forName("com.ex.Oracle");
+		System.out.println(C.getName());
+		System.out.println(C.newInstance());
+		Class C1=Class.forName("com.ex.MySql");
+		System.out.println(C1.getName());
+		System.out.println(C1.newInstance());
+		
+	}
 
-Vehicle nexon = new Vehicle();
-nexon.car_name= "Nexon";
-nexon.registration_number=1002;
-nexon.highest_speed=150;
-nexon.gear=6;
-nexon.price=700000;
-nexon.brand="suzuki";
-Vehicle.manufactured="Europe";
-System.out.println("Car:" + nexon.car_name);
-System.out.println("registration_number:" + nexon.registration_number);
-System.out.println("highest_speed:" + nexon.highest_speed);
-System.out.println( "gear:" + nexon.gear);
-System.out.println("price:" + nexon.price);
-System.out.println("brand:" + nexon.brand );
-System.out.println("manufactured:" + Vehicle.manufactured);
-}
 }
